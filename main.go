c5e136eb40c835a4e3956b02073b789d8a1d9dd0
jinbohu <1762858544@qq.com>
2023-03-06 19:04:14 +0800

    添加前端页面

--- main.go
+++ main.go
@@ -119,6 +119,19 @@ func main() {
 		statistics.GET("/rate/role", api.CountRoleRate)
 	}
 
+
+	// dir, _ := os.Getwd()
+	app.LoadHTMLGlob("dist/index.html") // 添加入口index.html
+	//r.LoadHTMLFiles("dist//*") // 添加资源路径
+	app.Static("/static/js", "./dist/static/js")                         // 添加资源路径
+	app.Static("/static/css", "./dist/static/css")                       // 添加资源路径
+	app.Static("/static/img", "./dist/static/img")                       // 添加资源路径
+	app.Static("/static/fonts", "./dist/static/fonts")                   // 添加资源路径
+	app.Static("/static/media", "./dist/static/media")                   // 添加资源路径
+	app.StaticFile("/favicon.ico", "./dist/favicon.ico")                 // 添加资源路径
+	app.StaticFile("/asset-manifest.json", "./dist/asset-manifest.json") // 添加资源路径
+	app.StaticFile("/", "./dist/index.html")                             //前端接口
+
 	app.Run(":" + configData.Port)
 
 }
